56c15f274006a37248ffcdbcb4639fe3b6abae6b
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2022-11-13 18:44:01 -0300

    Initial commit

---
+++ src/main/java/com/findus/findUs/FindUsApplication.java
@@ -0,0 +1,13 @@
+package com.findus.findUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findus/findUs/FindUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findus.findUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
